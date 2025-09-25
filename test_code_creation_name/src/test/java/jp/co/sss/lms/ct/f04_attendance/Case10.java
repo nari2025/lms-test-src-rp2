0f848f5209cdc1f1d0ef339f4964360c6f46ae22
nari2025 <eina-ichinari@kiseki.systems>
2025-09-25 17:20:32 +0900

    add: Case10.java
    日報の提出に関するテストコード追加
    
    ①～②はCase04.javaと同様
    ③上部メニューの勤怠リンクから勤怠管理画面に遷移
    期待値は以下の通り
    遷移先のURL：http://localhost:8080/lms/attendance/detail
    ④出勤ボタンを押下し、出勤時間を登録
    期待値は以下の通り
    class=alert alert-info alert-dismissible fade inの出現
    ⑤退勤ボタンを押下し、退勤時間を登録
    期待値は以下の通り
    class=alert alert-info alert-dismissible fade inの出現

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +42,108 @@ public class Case10 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// 指定のURLの画面を開く
+		goTo("http://localhost:8080/lms/");
+		//遷移したURLが正しいか確認
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/", currentUrl);
+		//タイトルが正しいか確認
+		String pageTitleString = WebDriverUtils.webDriver.getTitle();
+		assertEquals("ログイン | LMS", pageTitleString);
+
+		// ページのキャプチャを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		final WebElement loginId = WebDriverUtils.webDriver.findElement(By.id("loginId"));
+		final WebElement password = WebDriverUtils.webDriver.findElement(By.id("password"));
+		//初回ログイン済みのログインIDを入力し、TABキーを押下
+		loginId.clear();
+		loginId.sendKeys("StudentAA01");
+		loginId.sendKeys(Keys.TAB);
+		//初回ログイン済みのパスワードを入力し、Enterキーを押下
+		password.clear();
+		password.sendKeys("testTest1234");
+		password.sendKeys(Keys.ENTER);
+
+		// 遷移先のURLを取得し、正しいか確認
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		assertTrue(currentUrl.startsWith("http://localhost:8080/lms/course/detail"));
+		pageLoadTimeout(10);
+		//ユーザー名が表示されているか確認
+		WebElement smallElement = WebDriverUtils.webDriver.findElement(By.tagName("small"));
+		String userName = smallElement.getText();
+		assertEquals("ようこそ受講生ＡＡ１さん", userName);
+		// ページのキャプチャを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//勤怠リンクをクリック
+		final WebElement attendance = WebDriverUtils.webDriver.findElement(By.linkText("勤怠"));
+		attendance.click();
+		pageLoadTimeout(10);
+		//アラートダイアログ操作
+		Alert alert = WebDriverUtils.webDriver.switchTo().alert();
+		//ダイアログ「OK」ボタン押下
+		alert.accept();
+		pageLoadTimeout(10);
+		//遷移先のURLを確認
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		assertTrue(currentUrl.startsWith("http://localhost:8080/lms/attendance/detail"));
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+		//出勤ボタンをクリック
+		final WebElement punchIn = WebDriverUtils.webDriver.findElement(By.name("punchIn"));
+		punchIn.click();
+		//アラートダイアログ操作
+		Alert alert = WebDriverUtils.webDriver.switchTo().alert();
+		//ダイアログ「OK」ボタン押下
+		alert.accept();
+		pageLoadTimeout(10);
+		//属性値が変化しているか確認
+		String classText = WebDriverUtils.webDriver.findElement(By.xpath("//div[@class='main2clm']/div"))
+				.getAttribute("class");
+		assertEquals("alert alert-info alert-dismissible fade in", classText);
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
 	void test05() {
-		// TODO ここに追加
+		//出勤ボタンをクリック
+		final WebElement punchIn = WebDriverUtils.webDriver.findElement(By.name("punchOut"));
+		punchIn.click();
+		//アラートダイアログ操作
+		Alert alert = WebDriverUtils.webDriver.switchTo().alert();
+		//ダイアログ「OK」ボタン押下
+		alert.accept();
+		pageLoadTimeout(10);
+		//属性値が変化しているか確認
+		String classText = WebDriverUtils.webDriver.findElement(By.xpath("//div[@class='main2clm']/div"))
+				.getAttribute("class");
+		assertEquals("alert alert-info alert-dismissible fade in", classText);
+		getEvidence(new Object() {
+		});
+
 	}
 
 }
