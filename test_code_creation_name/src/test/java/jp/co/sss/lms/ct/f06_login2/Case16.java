6166f3fbe2825ff1d929a10580781f2e9d5d80ec
nari2025 <eina-ichinari@kiseki.systems>
2025-09-30 10:36:25 +0900

    add:case16.java
    初回ログインし、利用規約同意画面で不同意に対するテストコード追加
    
    ①～③はCase15.javaと同様
    ④パスワードを未入力で変更ボタンを押す
    期待値は以下の通り
    遷移先のURL：http://localhost:8080/lms/password/changePassword/change
    エラーメッセージの属性値：form-control errorInput
    エラーメッセージ：（現在のパスワード）現在のパスワードは必須です。
    （新しいパスワード）「パスワード」には半角英数字のみ使用可能です。また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。パスワードは必須です。
    （確認パスワード）確認パスワードは必須です。
    ⑤20文字以上の変更パスワードを入力し「変更」ボタンを押す
    期待値は以下の通り
    遷移先のURL：http://localhost:8080/lms/password/changePassword/change
    エラーメッセージの属性値：form-control errorInput
    エラーメッセージ：「パスワード」には半角英数字のみ使用可能です。また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。
    ⑥ポリシーに合わない変更パスワードを入力し「変更」ボタンを押す
    期待値は以下の通り
    遷移先のURL：http://localhost:8080/lms/password/changePassword/change
    エラーメッセージの属性値：form-control errorInput
    エラーメッセージ：「パスワード」には半角英数字のみ使用可能です。また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。
    ⑦一致しない確認パスワードを入力し「変更」ボタンを押す
    期待値は以下の通り
    遷移先のURL：http://localhost:8080/lms/password/changePassword/change
    エラーメッセージの属性値：form-control errorInput
    エラーメッセージ：パスワードと確認パスワードが一致しません。

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case16.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case16.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能②
@@ -35,49 +42,250 @@ public class Case16 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// 指定のURLの画面を開く
+		goTo("http://localhost:8080/lms/");
+		//遷移したURLが正しいか確認
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		assertEquals("http://localhost:8080/lms/", currentUrl);
+		//タイトルが正しいか確認
+		String pageTitleString = WebDriverUtils.webDriver.getTitle();
+		assertEquals("ログイン | LMS", pageTitleString);
+
+		// ページのキャプチャを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws InterruptedException {
+		final WebElement loginId = WebDriverUtils.webDriver.findElement(By.id("loginId"));
+		final WebElement password = WebDriverUtils.webDriver.findElement(By.id("password"));
+		//初回ログイン済みのログインIDを入力し、TABキーを押下
+		loginId.clear();
+		loginId.sendKeys("StudentAA03");
+		loginId.sendKeys(Keys.TAB);
+		//初回ログイン済みのパスワードを入力し、Enterキーを押下
+		password.clear();
+		password.sendKeys("StudentAA03");
+		password.sendKeys(Keys.ENTER);
+		Thread.sleep(3000);
+		// 遷移先のURLを取得し、正しいか確認
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		assertTrue(currentUrl.startsWith("http://localhost:8080/lms/user/agreeSecurity"));
+		pageLoadTimeout(10);
+		//ユーザー名が表示されているか確認
+		WebElement smallElement = WebDriverUtils.webDriver.findElement(By.tagName("small"));
+		String userName = smallElement.getText();
+		assertEquals("ようこそ受講生ＡＡ３さん", userName);
+
+		// ページのキャプチャを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックを入れ「次へ」ボタン押下")
 	void test03() {
-		// TODO ここに追加
+		// 同意するにチェックをいれ、次へを押す
+		final WebElement checkBox = WebDriverUtils.webDriver.findElement(By.xpath("//input[@value='1']"));
+		checkBox.click();
+		final WebElement nextBtn = WebDriverUtils.webDriver.findElement(By.xpath("//button[text()='次へ']"));
+		nextBtn.click();
+		//URLの確認
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		assertTrue(currentUrl.startsWith("http://localhost:8080/lms/password/changePassword"));
+		// ページのキャプチャを取得する
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 パスワードを未入力で「変更」ボタン押下")
 	void test04() {
-		// TODO ここに追加
+		final WebElement currentPassword = WebDriverUtils.webDriver.findElement(By.id("currentPassword"));
+		final WebElement password = WebDriverUtils.webDriver.findElement(By.id("password"));
+		final WebElement passwordConfirm = WebDriverUtils.webDriver.findElement(By.id("passwordConfirm"));
+		//全て未入力で変更ボタンを押下
+		currentPassword.clear();
+		currentPassword.sendKeys(Keys.TAB);
+		password.clear();
+		password.sendKeys(Keys.TAB);
+		passwordConfirm.clear();
+		passwordConfirm.sendKeys(Keys.ENTER);
+		WebElement changeButton = WebDriverUtils.webDriver.findElement(By.xpath("//button[text()='変更']"));
+		JavascriptExecutor js = (JavascriptExecutor) WebDriverUtils.webDriver;
+		js.executeScript("arguments[0].click();", changeButton);
+		// モーダル要素が出るのをまつ
+		visibilityTimeout(By.xpath("//div[@class='modal-dialog modal-dialog-center']"), 5);
+		// モーダル要素の変更ボタンを押す
+		WebElement modalElement = WebDriverUtils.webDriver
+				.findElement(By.xpath("//div[@class='modal-dialog modal-dialog-center']"));
+		WebElement upDateButton = modalElement.findElement(By.xpath(".//button[text()='変更']"));
+		upDateButton.click();
+
+		//URLの確認
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		assertTrue(currentUrl.startsWith("http://localhost:8080/lms/password/changePassword/change"));
+		//属性値が変化しているか確認
+		String currentPasswordError = WebDriverUtils.webDriver.findElement(By.id("currentPassword"))
+				.getAttribute("class");
+		assertEquals("form-control errorInput", currentPasswordError);
+		String passwordError = WebDriverUtils.webDriver.findElement(By.id("password")).getAttribute("class");
+		assertEquals("form-control errorInput", passwordError);
+		String passwordConfirmError = WebDriverUtils.webDriver.findElement(By.id("passwordConfirm"))
+				.getAttribute("class");
+		assertEquals("form-control errorInput", passwordConfirmError);
+		//エラーメッセージの確認
+		final WebElement currentPasswordErrorMsgElement = WebDriverUtils.webDriver
+				.findElement(By.xpath("//input[@id='currentPassword']/following-sibling::ul[1]//span"));
+		String currentPasswordErrorMsg = currentPasswordErrorMsgElement.getText();
+		assertEquals("現在のパスワードは必須です。", currentPasswordErrorMsg);
+
+		final WebElement passwordErrorElement = WebDriverUtils.webDriver
+				.findElement(By.xpath("//input[@id='password']/following-sibling::ul[1]//span"));
+		String passwordErrorMsg = passwordErrorElement.getText();
+		assertTrue(passwordErrorMsg
+				.contains("「パスワード」には半角英数字のみ使用可能です。また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。"));
+		assertTrue(passwordErrorMsg
+				.contains("パスワードは必須です。"));
+		final WebElement passwordConfirmErrorMsgElement = WebDriverUtils.webDriver
+				.findElement(By.xpath("//input[@id='passwordConfirm']/following-sibling::ul[1]//span"));
+		String passwordConfirmErrorMsg = passwordConfirmErrorMsgElement.getText();
+		assertEquals("確認パスワードは必須です。", passwordConfirmErrorMsg);
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 20文字以上の変更パスワードを入力し「変更」ボタン押下")
 	void test05() {
-		// TODO ここに追加
+		final WebElement currentPassword = WebDriverUtils.webDriver.findElement(By.id("currentPassword"));
+		final WebElement password = WebDriverUtils.webDriver.findElement(By.id("password"));
+		final WebElement passwordConfirm = WebDriverUtils.webDriver.findElement(By.id("passwordConfirm"));
+		//20文字以上の変更パスワードで変更ボタンを押下
+		currentPassword.clear();
+		currentPassword.sendKeys(Keys.TAB);
+		password.clear();
+		password.sendKeys("0123abcdABCD!#$%0123");
+		password.sendKeys(Keys.TAB);
+		passwordConfirm.clear();
+		passwordConfirm.sendKeys("0123abcdABCD!#$%0123");
+		passwordConfirm.sendKeys(Keys.ENTER);
+		WebElement changeButton = WebDriverUtils.webDriver.findElement(By.xpath("//button[text()='変更']"));
+		JavascriptExecutor js = (JavascriptExecutor) WebDriverUtils.webDriver;
+		js.executeScript("arguments[0].click();", changeButton);
+		// モーダル要素が出るのをまつ
+		visibilityTimeout(By.xpath("//div[@class='modal-dialog modal-dialog-center']"), 5);
+		// モーダル要素の変更ボタンを押す
+		WebElement modalElement = WebDriverUtils.webDriver
+				.findElement(By.xpath("//div[@class='modal-dialog modal-dialog-center']"));
+		WebElement upDateButton = modalElement.findElement(By.xpath(".//button[text()='変更']"));
+		upDateButton.click();
+
+		//URLの確認
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		assertTrue(currentUrl.startsWith("http://localhost:8080/lms/password/changePassword/change"));
+		//属性値が変化しているか確認
+		String classText = WebDriverUtils.webDriver.findElement(By.id("password")).getAttribute("class");
+		assertEquals("form-control errorInput", classText);
+		//エラーメッセージの確認
+		final WebElement errorMsgElement = WebDriverUtils.webDriver
+				.findElement(By.xpath("//input[@id='password']/following-sibling::ul[1]//span"));
+		String errorMsg = errorMsgElement.getText();
+		assertEquals("「パスワード」には半角英数字のみ使用可能です。また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。", errorMsg);
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 ポリシーに合わない変更パスワードを入力し「変更」ボタン押下")
 	void test06() {
-		// TODO ここに追加 
+		final WebElement currentPassword = WebDriverUtils.webDriver.findElement(By.id("currentPassword"));
+		final WebElement password = WebDriverUtils.webDriver.findElement(By.id("password"));
+		final WebElement passwordConfirm = WebDriverUtils.webDriver.findElement(By.id("passwordConfirm"));
+		//ポリシーに合わない変更パスワードを入力
+		currentPassword.clear();
+		currentPassword.sendKeys(Keys.TAB);
+		password.clear();
+		password.sendKeys("testtest");
+		password.sendKeys(Keys.TAB);
+		passwordConfirm.clear();
+		passwordConfirm.sendKeys("testtest");
+		passwordConfirm.sendKeys(Keys.ENTER);
+		WebElement changeButton = WebDriverUtils.webDriver.findElement(By.xpath("//button[text()='変更']"));
+		JavascriptExecutor js = (JavascriptExecutor) WebDriverUtils.webDriver;
+		js.executeScript("arguments[0].click();", changeButton);
+		// モーダル要素が出るのをまつ
+		visibilityTimeout(By.xpath("//div[@class='modal-dialog modal-dialog-center']"), 5);
+		// モーダル要素の変更ボタンを押す
+		WebElement modalElement = WebDriverUtils.webDriver
+				.findElement(By.xpath("//div[@class='modal-dialog modal-dialog-center']"));
+		WebElement upDateButton = modalElement.findElement(By.xpath(".//button[text()='変更']"));
+		upDateButton.click();
+
+		//URLの確認
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		assertTrue(currentUrl.startsWith("http://localhost:8080/lms/password/changePassword/change"));
+		//属性値が変化しているか確認
+		String classText = WebDriverUtils.webDriver.findElement(By.id("password")).getAttribute("class");
+		assertEquals("form-control errorInput", classText);
+		//エラーメッセージの確認
+		final WebElement errorMsgElement = WebDriverUtils.webDriver
+				.findElement(By.xpath("//input[@id='password']/following-sibling::ul[1]//span"));
+		String errorMsg = errorMsgElement.getText();
+		assertEquals("「パスワード」には半角英数字のみ使用可能です。また、半角英大文字、半角英小文字、数字を含めた8～20文字を入力してください。", errorMsg);
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 一致しない確認パスワードを入力し「変更」ボタン押下")
 	void test07() {
-		// TODO ここに追加
+		final WebElement currentPassword = WebDriverUtils.webDriver.findElement(By.id("currentPassword"));
+		final WebElement password = WebDriverUtils.webDriver.findElement(By.id("password"));
+		final WebElement passwordConfirm = WebDriverUtils.webDriver.findElement(By.id("passwordConfirm"));
+		//一致しない確認パスワードを入力
+		currentPassword.clear();
+		currentPassword.sendKeys("StudentAA03");
+		currentPassword.sendKeys(Keys.TAB);
+		password.clear();
+		password.sendKeys("testTest1234");
+		password.sendKeys(Keys.TAB);
+		passwordConfirm.clear();
+		passwordConfirm.sendKeys("testTest5678");
+		passwordConfirm.sendKeys(Keys.ENTER);
+		WebElement changeButton = WebDriverUtils.webDriver.findElement(By.xpath("//button[text()='変更']"));
+		JavascriptExecutor js = (JavascriptExecutor) WebDriverUtils.webDriver;
+		js.executeScript("arguments[0].click();", changeButton);
+		// モーダル要素が出るのをまつ
+		visibilityTimeout(By.xpath("//div[@class='modal-dialog modal-dialog-center']"), 5);
+		// モーダル要素の変更ボタンを押す
+		WebElement modalElement = WebDriverUtils.webDriver
+				.findElement(By.xpath("//div[@class='modal-dialog modal-dialog-center']"));
+		WebElement upDateButton = modalElement.findElement(By.xpath(".//button[text()='変更']"));
+		upDateButton.click();
+
+		//URLの確認
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		assertTrue(currentUrl.startsWith("http://localhost:8080/lms/password/changePassword/change"));
+		//属性値が変化しているか確認
+		String classText = WebDriverUtils.webDriver.findElement(By.id("password")).getAttribute("class");
+		assertEquals("form-control errorInput", classText);
+		//エラーメッセージの確認
+		final WebElement errorMsgElement = WebDriverUtils.webDriver
+				.findElement(By.xpath("//input[@id='password']/following-sibling::ul[1]//span"));
+		String errorMsg = errorMsgElement.getText();
+		assertEquals("パスワードと確認パスワードが一致しません。", errorMsg);
+		getEvidence(new Object() {
+		});
 	}
 
 }
